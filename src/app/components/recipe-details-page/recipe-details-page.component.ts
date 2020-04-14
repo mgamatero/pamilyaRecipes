802001db8d341d3b38708870aac7809d42089df1
Mike Gamatero <mgamatero@yahoo.com>
2020-04-14 12:48:53 -0700

    Recipe details page. Id from paramsID works

--- src/app/components/recipe-details-page/recipe-details-page.component.ts
+++ src/app/components/recipe-details-page/recipe-details-page.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+
+import { Recipe } from '../../models/recipe';
 
 @Component({
   selector: 'app-recipe-details-page',
   templateUrl: './recipe-details-page.component.html',
-  styleUrls: ['./recipe-details-page.component.css']
+  styleUrls: ['./recipe-details-page.component.css'],
 })
 export class RecipeDetailsPageComponent implements OnInit {
+recipeId;
 
-  constructor() { }
+
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
+        //This is the id from params
+        this.route.paramMap.subscribe((params: ParamMap) => {
+          this.recipeId = params.get("id");
+        });
   }
-
 }
