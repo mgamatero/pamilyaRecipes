365ccaa944414e68414b8323ba7b1c9c2ad7796c
Mike Gamatero <mgamatero@yahoo.com>
2020-04-15 10:20:58 -0700

    final before deploy to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
-export const environment = {
-  production: true
-};
+
+  export const environment = {
+    production: true,
+    firebase : {
+      apiKey: "AIzaSyDlVCaMD83-3m4dR5UEo1ylMwdg7enUgFE",
+      authDomain: "mypamilyarecipes.firebaseapp.com",
+      databaseURL: "https://mypamilyarecipes.firebaseio.com",
+      projectId: "mypamilyarecipes",
+      storageBucket: "mypamilyarecipes.appspot.com",
+      messagingSenderId: "549432231169",
+      appId: "1:549432231169:web:bb6953acdff9d06f830700",
+      measurementId: "G-87D9KPZGNG"
+    }
+  };
+
