b5643a335e9c2103df4e6fe92867554adc32fb43
Mike Gamatero <mgamatero@yahoo.com>
2020-04-21 11:16:35 -0700

    edit recipe page - form and preview pane added.  Form and pane are connected

--- src/app/components/edit-delete-editonly-recipes-page/edit-delete-editonly-recipes-page.component.ts
+++ src/app/components/edit-delete-editonly-recipes-page/edit-delete-editonly-recipes-page.component.ts
@@ -7,6 +7,13 @@ import { analytics } from 'firebase';
 
 import { Recipe } from '../../models/recipe';
 import { RecipeService } from '../../services/recipe.service';
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+  FormArray,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-edit-delete-editonly-recipes-page',
@@ -16,11 +23,13 @@ import { RecipeService } from '../../services/recipe.service';
 export class EditDeleteEditonlyRecipesPageComponent implements OnInit {
   recipeId: string;
   indivRecipeToUpdate$: Recipe;
+  recipeForm: FormGroup;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private recipeService: RecipeService
+    private recipeService: RecipeService,
+    private fb: FormBuilder, private db: AngularFirestore
   ) {
     //This is the id from params
     this.route.paramMap.subscribe((params: ParamMap) => {
@@ -33,10 +42,103 @@ export class EditDeleteEditonlyRecipesPageComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipeForm= this.fb.group({
+      name: ['', Validators.required],
+      addedBy: ['', Validators.required],
+      type: ['', Validators.required],
+      image: ['',Validators.required],
+      ingredients: this.fb.array([this.fb.control('',Validators.required)]),
+      instructions: this.fb.array([this.fb.control('',Validators.required)]),
+    })
+  }
 
   updateItem(recipe:Recipe){
     this.recipeService.updateRecipe(recipe);
     // this.clearState();
   }
+
+  get ingredients() {
+  return this.recipeForm.get('ingredients') as FormArray;
 }
+
+addNewIngredient() {
+  this.ingredients.push(this.fb.control(''));
+}
+
+get instructions() {
+  return this.recipeForm.get('instructions') as FormArray;
+}
+
+addNewInstruction() {
+  this.instructions.push(this.fb.control(''));
+}
+
+submitForm() {
+  console.warn(this.recipeForm.value.name);
+  this.recipeService.addRecipe(this.recipeForm.value);
+  this.clearForm();
+  alert('Recipe added');
+  this.router.navigate(['recipes']);
+}
+
+clearForm() {
+  this.recipeForm.reset();
+  const ingredientControls = <FormArray>(
+    this.recipeForm.controls['ingredients']
+  );
+  for (let i = ingredientControls.length - 1; i >= 0; i--) {
+    ingredientControls.removeAt(i);
+  }
+  const instructionControls = <FormArray>(
+    this.recipeForm.controls['instructions']
+  );
+  for (let i = instructionControls.length - 1; i >= 0; i--) {
+    instructionControls.removeAt(i);
+  }
+}
+}
+
+
+
+
+
+// get ingredients() {
+//   return this.recipeForm.get('ingredients') as FormArray;
+// }
+
+// addNewIngredient() {
+//   this.ingredients.push(this.fb.control(''));
+// }
+
+// get instructions() {
+//   return this.recipeForm.get('instructions') as FormArray;
+// }
+
+// addNewInstruction() {
+//   this.instructions.push(this.fb.control(''));
+// }
+
+// submitForm() {
+//   console.warn(this.recipeForm.value.name);
+//   this.recipeService.addRecipe(this.recipeForm.value);
+//   this.clearForm();
+//   alert('Recipe added');
+//   this.router.navigate(['recipes']);
+// }
+
+// clearForm() {
+//   this.recipeForm.reset();
+//   const ingredientControls = <FormArray>(
+//     this.recipeForm.controls['ingredients']
+//   );
+//   for (let i = ingredientControls.length - 1; i >= 0; i--) {
+//     ingredientControls.removeAt(i);
+//   }
+//   const instructionControls = <FormArray>(
+//     this.recipeForm.controls['instructions']
+//   );
+//   for (let i = instructionControls.length - 1; i >= 0; i--) {
+//     instructionControls.removeAt(i);
+//   }
+// }
